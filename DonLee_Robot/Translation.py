5c42ad94e90c9a8ae76c1e683e9a58328dedc42c
godgroot <85842733+godgroot@users.noreply.github.com>
2021-10-17 18:11:25 +0530

    Create Translation.py

---
+++ DonLee_Robot/Translation.py
@@ -0,0 +1,53 @@
+import os
+class Translation(object):
+  
+    START_TEXT = """<b>🙋‍♂️Hey {}!!</b>
+
+<b>Am just An Avenger....😉</b>
+
+<b>All Avengers Movie Available On @CINEMAZIP🔥🔥😝</b>
+
+<b>SHARE OUR GROUP FOR MORE...</b>
+
+<b>WE HAVE INSTAGRAM TOO🤪</b>
+
+<b>Maintained By @im_godgroot</b>"""    
+    
+    HELP_TEXT = """
+<b><u>Notice</u></b>
+<code>Imdb Poster Imdb is available on this bot
+Rating Not Available</codd> 
+
+<b><u>Bot Commands (Works Only In Groups)</u></b>
+
+☞ <code>/add chat_id</code> - <b>To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)</b>
+  
+☞ <code>/del chat_id</code> - <b>To disconnect A Group With A Channel</b>
+     
+☞ <code>/delall</code>  - <b>This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB</b>
+    
+☞ <code>/settings</code> -  <b>This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly</b>
+
+   ☞ <code>Channel</code> - <b>Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls</b>
+            
+   ☞ <code>Filter Types</code> - <b>Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart</b>
+
+   ☞ <code>Configure</code> - <b>Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results</b>
+            
+   ☞ <code>Status</code> - <b>Button Will Shows The Stats Of Your Channel</b>
+
+Maintained By @Mo_Tech_YT"""
+    
+    ABOUT_TEXT = """
+<b>➥ 🤖Bot</b> : <b>Im Hulk </b>
+    
+<b>➥ 😎Creator</b> : <b>@im_godgroot</b> 
+
+<b>➥ 👨‍💻Editor</b> : <b>@im_godgroot</b>
+
+<b>➥ 🗣️Language</b> : <b>Python3</b>
+
+<b>➥ 📚Library</b> : <b>Pyrogram Asyncio 1.13.0</b>
+
+<b>➥ 📖Source Code</b> : <b><a href="https://t.me/share/url?url=https://t.me/cinemazip">💥Click Me</a></b>
+"""
